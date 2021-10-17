d61f5aa037f7024658868a5fb07a44789b63df21
tanvesh01 <sarvetanvesh01@gmail.com>
2021-10-18 00:28:13 +0530

    docs: add AnimatedCounter to examples

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css';
 import AnimatingList from './ExampleComponents/AnimatingList/AnimatingList';
 
+import AnimatedCounter from "./ExampleComponents/AnimatedCounter/AnimatedCounter"
+
 function App() {
-    return <AnimatingList />;
+    return <><AnimatingList />;
+	
+	<AnimatedCounter />
+	</>
 }
 
 export default App;
